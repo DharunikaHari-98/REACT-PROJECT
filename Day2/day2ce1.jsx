12ad7f9503d51bb21e8de4927243f1c011629ab0
DharunikaHari-98 <150768313+DharunikaHari-98@users.noreply.github.com>
2024-08-08 16:08:40 +0530

    Add files via upload

---
+++ Day2/day2ce1.jsx
@@ -0,0 +1,24 @@
+
+function World()
+{
+   
+       
+            alert("This is good");
+            console.log("hello");
+            
+        
+    
+}
+
+
+function App()
+{
+    return(
+        <div className="App">
+        
+            <button onClick={World}>CE1</button>
+          
+        </div>
+    )
+}
+export default App;
