70bf8611df4c51a33a8cd810143291e564adb1dc
DharunikaHari-98 <150768313+DharunikaHari-98@users.noreply.github.com>
2024-11-13 17:49:42 +0530

    Update CE.jsx

--- DAY7/CE.jsx
+++ DAY7/CE.jsx
@@ -1,54 +1,23 @@
 
-import React, { useEffect, useState } from 'react';
-import { Avatar, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
-import { getAllUsers } from '../services/UserApi';
-
-const UserList = () => {
-
-    const [userList, setUserList] = useState([]);
-
-    useEffect(() => {
-        const fetchData = () => getAllUsers()
-            .then((res) => setUserList(res.data))
-            .catch((error) => console.log(error));
-        fetchData();
-    }, [])
-
-    return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: '#1d1d1d', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-            {
-                userList.map((item) => (
-                    <ListItem key={item.id} alignItems="flex-start">
-                        <ListItemAvatar>
-                            <Avatar alt={item.name} src={item.name} />
-                        </ListItemAvatar>
-                        <ListItemText sx={{ display: 'flex', flexDirection: 'column' }}
-                            primary={
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body2"
-                                    color="#f1f1f1"
-                                >
-                                    {item.name}
-                                </Typography>
-                            }
-                            secondary={
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body2"
-                                    color="#f1f1f1"
-                                >
-                                    {item.email}
-                                </Typography>
-                            }
-                        />
-                    </ListItem>
-                ))
-            }
-        </List>
-    )
+import React,{useState,useEffect} from 'react';
+import Axios from 'axios';
+function Get()
+{
+    const[book,setBook]=useState([]);
+    useEffect(()=>{
+Axios.get('http://localhost:3000/books')
+.then(response=>setBook(response.data))
+    })
+return(
+    <div>
+<h3>My Book List</h3>
+{book.map((bo)=>
+<div key={bo.id}>
+    <div>{bo.title}</div>
+    <div>{bo.author}</div>
+    </div>
+)}
+    </div>
+)
 }
-
-export default UserList;
+export default Get;
